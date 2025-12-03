d85ecbd278940067503603cf59d654926f0832df
Xian Dev <xiandev25@gmail.com>
2025-12-03 23:01:25 +0100

    Add a StateFlow property for the GameUiState to the ViewModel

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class GameViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(GameUiState())
 }
