56fd7026bd392cce6513519f61f29722ec67c39d
Xian Dev <xiandev25@gmail.com>
2025-12-03 23:09:11 +0100

    Add a backing property for the _uiState property
    ...making it accessible out of the ViewModel.

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -2,8 +2,11 @@ package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel : ViewModel() {
 
     private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
 }
